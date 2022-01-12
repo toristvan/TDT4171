ea4d48a947b8d5eeabb9383d0346d89b3ebbbc03
Tor Istvan Stadler Kjetsaa <tor.istvan@solutionseeker.no>
2022-01-12 15:48:24 +0100

    unignore a pylint thingy

--- ass4/skeleton_v2.py
+++ ass4/skeleton_v2.py
@@ -260,7 +260,6 @@ def main():
         x_test_big_nonsep = np.loadtxt(
             "data_big_nonsep_test.csv", delimiter="\t", usecols=(0, 1)
         )
-        # trunk-ignore(pylint/W0612)
         y_test_big_nonsep = np.loadtxt(
             "data_big_nonsep_test.csv", delimiter="\t", usecols=(2,)
         )
